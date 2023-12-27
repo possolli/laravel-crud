a27120327a71e0df075638d819e11cc3fba32793
Zé Passolli <joseolavopossolli04@gmail.com>
2023-12-26 22:48:42 -0300

    contact-crud

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreContactRequest;
+use App\Http\Requests\UpdateContactRequest;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        $contact = Contact::all();
+        return response()->json($contact);
+    }
+
+    public function store(StoreContactRequest $request)
+    {
+        $data = $request->validated();
+        Contact::create($data);
+        return to_route('/');
+    }
+
+    public function update(UpdateContactRequest $request, Contact $contact)
+    {
+        $data = $request->validated();
+        $contact->update($data);
+    }
+
+    public function destroy(Contact $contact)
+    {
+        $contact->delete();
+    }
+}
---
+++ app/Http/Requests/UpdateContactRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateContactRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'fullname' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'max:255'],
+            'subject' => ['required' , 'string', 'max:255'],
+            'message' => ['required' , 'string', 'max:10000']
+        ];
+    }
+}
